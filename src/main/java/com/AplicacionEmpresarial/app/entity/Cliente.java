c9983f8edcee631d5c8c6a7f8889319784fe835b
@F3rn4nd0x013 <fvasquezb@uts.edu.co>
2022-06-06 02:01:59 -0500

    AplicacionEmpresarial

---
+++ src/main/java/com/AplicacionEmpresarial/app/entity/Cliente.java
@@ -0,0 +1,88 @@
+package com.AplicacionEmpresarial.app.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	private String nombre;
+
+	@NotEmpty
+	private String mascota;
+
+	@NotEmpty
+	private String telefono;
+
+	@NotNull
+	@Column(name = "fecha")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date fecha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMascota() {
+		return mascota;
+	}
+
+	public void setMascota(String mascota) {
+		this.mascota = mascota;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+}
---
+++ src/test/java/com/AplicacionEmpresarial/app/AplicacionEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AplicacionEmpresarial.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
